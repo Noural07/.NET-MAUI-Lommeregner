6af07aa7c8d35111702fd2e002bed454270e7236
Noural07 <nour.habbal.3@gmail.com>
2024-05-01 13:23:37 +0200

    a
    a

---
+++ Lommeregner/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Lommeregner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Lommeregner/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Lommeregner
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
